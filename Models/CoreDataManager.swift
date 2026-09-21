b0c0689ae8b2c1a6e945965bc4834db3d1a828d1
Minh Duc <minhducworking05@gmail.com>
2026-09-21 22:36:56 +0700

    feat: add Core Data stack and CDSubscription data model

--- Models/CoreDataManager.swift
+++ Models/CoreDataManager.swift
@@ -41,7 +41,7 @@ class CoreDataManager {
 }
 
 @objc(CDSubscription)
-public class CDSubscription: NSManagedObject {
+public class CDSubscription: NSManagedObject, Identifiable {
     @NSManaged public var id: UUID?
     @NSManaged public var name: String?
     @NSManaged public var price: Double
